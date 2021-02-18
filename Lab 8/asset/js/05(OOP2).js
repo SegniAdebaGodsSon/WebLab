116d0804f446ecace5e54741ff738a1087e079aa
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2021-02-18 10:06:19 +0400

    OOP2 exercise done

--- Lab 8/asset/js/05(OOP2).js
+++ Lab 8/asset/js/05(OOP2).js
@@ -8,9 +8,12 @@ const PersonObj = {
 
 //2. Create Theon Object using Person Template
 
+let theon = PersonObj
+
 
 //3. Add firstName[Theon] and lastName[Greyjoy] 
-
+theon.firstName = "Theon"
+theon.lastName = "Greyjoy"
 
 //4. Display the greeting on Theon Object [remove the string when you have the object]
-Obj_C2.innerHTML = "Theon.greeting()";
+Obj_C2.innerHTML = theon.greeting();
