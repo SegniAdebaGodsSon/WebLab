a6b1126c9a38f13b489ae1e6ab678347cb2aa776
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2021-02-18 10:23:03 +0400

    Exception exercise done

--- Lab 8/asset/js/06(exception).js
+++ Lab 8/asset/js/06(exception).js
@@ -19,16 +19,23 @@ function divideNo() {
 
     // 1. Define a try block 
 
+   
+
     // 2. put this two lines of code inside the try block 
 
-    const result = performDivision(no1, no2);
-
-    res.innerHTML = result;
 
 
     //4.Catch the exception and display error message [remove the string when you have the error variable]
-    res.innerHTML = "error";
+    
 
+    let result = null;
+    try {
+        result = performDivision(no1, no2);
+    } catch (error) {
+        result = error;
+    } finally {
+        res.innerHTML = result;
+    }
 }
 
 
