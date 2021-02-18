1a13db21ef8f377d7bafaac56be8b70aa0b53838
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2021-02-18 10:09:16 +0400

    OOP3 exercise done

--- Lab 8/asset/js/05(OOP3).js
+++ Lab 8/asset/js/05(OOP3).js
@@ -1,7 +1,6 @@
 // 1. Create a Person_ES6 Class 
 
 
-
 //2. Create the constructor that initialize firstName and lastName
 
 
@@ -10,7 +9,19 @@
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
 
+class Person3{
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    greeting(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+let Bran = new Person3('Bran', 'Stark')
 
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
